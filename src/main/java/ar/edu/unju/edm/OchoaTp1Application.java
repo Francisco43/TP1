67f0c9e2eb2bfd1aeafff2e73a1dbc71a741c218
Francisco43 <84818198+Francisco43@users.noreply.github.com>
2021-07-12 19:23:46 -0300

    Agregando cosas al trabajo práctico número 1

---
+++ src/main/java/ar/edu/unju/edm/OchoaTp1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OchoaTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OchoaTp1Application.class, args);
+	}
+
+}
